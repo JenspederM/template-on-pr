7fdb8c1f2bd22e09ab0169ab424f2ff8d42e9e5b
Jens Peder Meldgaard <jenspederm@gmail.com>
2023-11-05 12:42:17 +0100

    add or func

--- src/template_on_pr/main.py
+++ src/template_on_pr/main.py
@@ -1,2 +1,15 @@
+"""This is a template for a function that will be called on PRs."""
+
+
 def myfunc():
+    """Return a message.
+
+    Returns:
+        str: Hello World!
+    """
     return "Hello World!"
+
+
+def pr_func():
+    """Print a message when called on PRs."""
+    return "This is a function that will be called on PRs"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
-from template_on_pr.main import myfunc
+from template_on_pr.main import myfunc, pr_func
 
 
 def test_myfunc():
     assert myfunc() == "Hello World!"
+
+
+def test_pr_func():
+    assert pr_func() == "This is a function that will be called on PRs"
